d58aa7f4af3c1fd984c4fe2e64ace6c1547efd7e
root <root@ampere01.pouliot.net>
2019-10-02 10:22:47 -0400

    fixing main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
-module "raw_centos_img" {
+module "centos_aarch46" {
   source = "./centos"
   enable_centos_7_aarch64_qcow2 = true
   enable_centos_7_aarch64_raw = false
 }
-module "raw_debian_img" {
+module "debian_arm64" {
   source = "./debian"
   enable_debian_9_arm64_qcow2 = true
   enable_debian_9_arm64_raw = false
   enable_debian_10_arm64_qcow2 = true
   enable_debian_10_arm64_raw = false
 }
-module "raw_fedora_img" {
+module "fedora_aarch64" {
   source = "./fedora"
   enable_fedora_server_29_aarch64_qcow2 = true
   enable_fedora_server_29_aarch64_raw = false
   enable_fedora_server_30_aarch64_qcow2 = true
   enable_fedora_server_30_aarch64_raw = false
 }
-#module "raw_freebsd_img" {
+#module "freebsd_aarch64" {
 #  source = "./freebsd"
 #  count = var.enable_freebsd ? 1:0
 #  enable_freebsd = true
 #}
-module "raw_opensuse_img" {
+module "opensuse_aarch64" {
   source = "./opensuse"
   enable_opensuse_151_aarch64_qcow2 = true
   enable_opensuse_151_aarch64_raw = false
 }
-module "raw_ubuntu_img" {
+module "ubuntu_arm64" {
   source = "./ubuntu"
   enable_ubuntu_1804_bionic_arm64_qcow2 = true
   enable_ubuntu_1804_bionic_arm64_raw = false
