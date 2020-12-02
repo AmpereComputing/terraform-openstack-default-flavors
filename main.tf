db005e1cd79dc870ce406500237565ce52f90a0c
Peter Pouliot <peter@pouliot.net>
2020-12-02 06:53:07 -0800

    removing unneeded lines and fixing is_public to correct values

--- main.tf
+++ main.tf
@@ -4,12 +4,7 @@ resource "openstack_compute_flavor_v2" "m1-tiny" {
   ram   = "1024"
   vcpus = "1"
   disk  = "1"
-  is_public = "1"
-
-# extra_specs = {
-#   "hw:cpu_policy"        = "CPU-POLICY",
-#   "hw:cpu_thread_policy" = "CPU-THREAD-POLICY"
-# }
+  is_public = true
 }
 resource "openstack_compute_flavor_v2" "m1-small" {
   count = var.enable_m1_small ? 1:0
@@ -17,12 +12,7 @@ resource "openstack_compute_flavor_v2" "m1-small" {
   ram   = "2048"
   vcpus = "1"
   disk  = "20"
-  is_public = "1"
-
-# extra_specs = {
-#   "hw:cpu_policy"        = "CPU-POLICY",
-#   "hw:cpu_thread_policy" = "CPU-THREAD-POLICY"
-# }
+  is_public = true
 }
 resource "openstack_compute_flavor_v2" "m1-medium" {
   count = var.enable_m1_medium ? 1:0
@@ -30,12 +20,7 @@ resource "openstack_compute_flavor_v2" "m1-medium" {
   ram   = "4096"
   vcpus = "2"
   disk  = "40"
-  is_public = "1"
-
-# extra_specs = {
-#   "hw:cpu_policy"        = "CPU-POLICY",
-#   "hw:cpu_thread_policy" = "CPU-THREAD-POLICY"
-# }
+  is_public = true
 }
 resource "openstack_compute_flavor_v2" "m1-large" {
   count = var.enable_m1_large ? 1:0
@@ -43,12 +28,7 @@ resource "openstack_compute_flavor_v2" "m1-large" {
   ram   = "8192"
   vcpus = "4"
   disk  = "80"
-  is_public = "1"
-
-# extra_specs = {
-#   "hw:cpu_policy"        = "CPU-POLICY",
-#   "hw:cpu_thread_policy" = "CPU-THREAD-POLICY"
-# }
+  is_public = true
 }
 resource "openstack_compute_flavor_v2" "m1-xlarge" {
   count = var.enable_m1_xlarge ? 1:0
@@ -56,10 +36,5 @@ resource "openstack_compute_flavor_v2" "m1-xlarge" {
   ram   = "16834"
   vcpus = "8"
   disk  = "160"
-  is_public = "1"
-
-# extra_specs = {
-#   "hw:cpu_policy"        = "CPU-POLICY",
-#   "hw:cpu_thread_policy" = "CPU-THREAD-POLICY"
-# }
+  is_public = true
 }
